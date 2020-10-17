a3f8350b6b0af193c9ae1d515bcd05badfa02d89
griends1 <Steelpan1>
2020-10-17 13:49:34 +0200

    Added repo & homefragment

--- app/src/main/java/com/example/madlevel7task2/UI/HomeFragment.kt
+++ app/src/main/java/com/example/madlevel7task2/UI/HomeFragment.kt
@@ -1,10 +1,13 @@
-package com.example.madlevel7task2
+package com.example.madlevel7task2.UI
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.madlevel7task2.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -12,8 +15,8 @@ import android.view.ViewGroup
 class HomeFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
@@ -21,6 +24,10 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        btn_start_quest.setOnClickListener{onStartClick()}
+    }
 
+    private fun onStartClick() {
+        findNavController().navigate(R.id.questionsFragment)
     }
 }
